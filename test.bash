352ffd812ef09833c2341f7d6500768604b4752a
yukihanada <hanada@hoge.com>
2022-10-27 12:51:37 +0900

    Add test.bash

--- test.bash
+++ test.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 yukihanada
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK
+exit $rse
 
 
